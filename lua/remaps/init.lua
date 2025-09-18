227ecb47f64bf833fb694fdbc7d4caaa79271877
Eric L <60950660+elarswa@users.noreply.github.com>
2025-09-18 17:52:33 -0600

    feat: remaps for front/end of line

--- lua/remaps/init.lua
+++ lua/remaps/init.lua
@@ -11,6 +11,8 @@ vim.keymap.set("n", "N", "Nzz") -- center after find jump reverse
 vim.keymap.set("n", "J", "mzJ`z") -- don't move cursor on line join
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "H", "0")
+vim.keymap.set("n", "L", "$")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
 
@@ -22,7 +24,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set("v", "p", "pgvy") -- retain yank buffer when pasting over selection
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
+vim.keymap.set("v", "H", "0")
+vim.keymap.set("v", "L", "$")
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
