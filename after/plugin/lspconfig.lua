6b535bf942a3a2fd6edf0c675c5f68648cb246e9
Eric L <60950660+elarswa@users.noreply.github.com>
2025-09-22 14:26:47 -0600

    feat: remaps plus lsp hover change

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
 
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
     vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
